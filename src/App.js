034a8b67f32f11021a5fc6dedbb5144d10f5f5e1
Stealthy20 <kjellin.fredrik@live.se>
2023-02-10 11:44:06 +0000

    Add a form for create a review

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./api/axiosDefaults";
 import SignInForm from "./pages/auth/SignInForm";
 import SignUpForm from "./pages/auth/SignUpForm";
 import CategoryBar from "./components/CategoryBar";
+import ReviewCreateForm from "./pages/reviews/ReviewCreateForm";
 
 
 function App() {
@@ -36,7 +37,7 @@ function App() {
           />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
-          {/* <Route exact path="/posts/create" render={() => <PostCreateForm />} /> */}
+          <Route exact path="/reviews/create" render={() => <ReviewCreateForm />} />
           {/* <Route exact path="/posts/:id" render={() => <PostPage />} /> */}
           {/* <Route exact path="/posts/:id/edit" render={() => <PostEditForm />} /> */}
           {/* <Route exact path="/profiles/:id" render={() => <ProfilePage />} /> */}
