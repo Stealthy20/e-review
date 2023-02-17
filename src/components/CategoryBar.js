025cbe81837ac902687642972c38088739a356fe
Stealthy20 <kjellin.fredrik@live.se>
2023-02-17 13:08:42 +0000

    Add respinsivness with dropdwonbar to categorybar

--- src/components/CategoryBar.js
+++ src/components/CategoryBar.js
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "../styles/CategoryBar.module.css";
+import Nav from "react-bootstrap/Nav";
+import Dropdown from "react-bootstrap/Dropdown";
+import NavItem from "react-bootstrap/NavItem";
+import NavLink from "react-bootstrap/NavLink";
 
 const CategoryBar = (props) => {
   const { setFilter } = props;
@@ -10,49 +14,112 @@ const CategoryBar = (props) => {
   };
 
   return (
-    <div className={styles.CategoryBar}>
-        <span
-        className={styles.NavLink}
-        onClick={() => window.location.reload()}
-      >
-        All
-      </span>
-      <span
-        className={styles.NavLink}
-        onClick={(e) => handleClick(e)}
-      >
-        Tv
-      </span>
-      <span
-        className={styles.NavLink}
-        onClick={(e) => handleClick(e)}
-      >
-        Mobile Phone
-      </span>
-      <span
-        className={styles.NavLink}
-        onClick={(e) => handleClick(e)}
-      >
-        Tablet
-      </span>
-      <span
-        className={styles.NavLink}
-        onClick={(e) => handleClick(e)}
-      >
-        Tv Accessories
-      </span>
-      <span
-        className={styles.NavLink}
-        onClick={(e) => handleClick(e)}
-      >
-        Mobile Phone Accessories
-      </span>
-      <span
-        className={styles.NavLink}
-        onClick={(e) => handleClick(e)}
-      >
-        Tablet Accessories
-      </span>
+    <div>
+      <div className={`d-none d-md-block ${styles.CategoryBar}`}>
+        <Nav defaultActiveKey="/home" as="ul">
+          <Nav.Item as="li">
+            <Nav.Link
+              className={styles.NavLink}
+              onClick={() => window.location.reload()}
+            >
+              All
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link
+              className={styles.NavLink}
+              onClick={(e) => handleClick(e)}
+            >
+              Tv
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link
+              className={styles.NavLink}
+              onClick={(e) => handleClick(e)}
+            >
+              Mobile Phone
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link
+              className={styles.NavLink}
+              onClick={(e) => handleClick(e)}
+            >
+              Tablet
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link
+              className={styles.NavLink}
+              onClick={(e) => handleClick(e)}
+            >
+              Tv Accessories
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link
+              className={styles.NavLink}
+              onClick={(e) => handleClick(e)}
+            >
+              Mobile Phone Accessories
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link
+              className={styles.NavLink}
+              onClick={(e) => handleClick(e)}
+            >
+              Tablet Accessories
+            </Nav.Link>
+          </Nav.Item>
+          </Nav>
+          </div>
+          <Dropdown className="d-md-none" as={NavItem}>
+            <Dropdown.Toggle className="font-weight-bold" as={NavLink}>Categories</Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                className={styles.NavLink}
+                onClick={() => window.location.reload()}
+              >
+                All
+              </Dropdown.Item>
+              <Dropdown.Item
+                className={styles.NavLink}
+                onClick={(e) => handleClick(e)}
+              >
+                Tv
+              </Dropdown.Item>
+              <Dropdown.Item
+                className={styles.NavLink}
+                onClick={(e) => handleClick(e)}
+              >
+                Mobile Phone
+              </Dropdown.Item>
+              <Dropdown.Item
+                className={styles.NavLink}
+                onClick={(e) => handleClick(e)}
+              >
+                <Dropdown.Item
+                  className={styles.NavLink}
+                  onClick={(e) => handleClick(e)}
+                ></Dropdown.Item>
+                Tablet
+              </Dropdown.Item>
+              <Dropdown.Item
+                className={styles.NavLink}
+                onClick={(e) => handleClick(e)}
+              >
+                Mobile Phone Accessories
+              </Dropdown.Item>
+              <Dropdown.Item
+                className={styles.NavLink}
+                onClick={(e) => handleClick(e)}
+              >
+                Tablet Accessories
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
     </div>
   );
 };
